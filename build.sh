b1707138a4efc1ec10b620cd6ba7b7b3946367cf
wizhou <coucou@romaingoetz.fr>
2020-07-21 22:23:46 +0200

    Update du build.sh
    Update du build.sh

--- build.sh
+++ build.sh
@@ -2,32 +2,44 @@
 
 if [ "`git status -s`" ]
 then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
+  echo "---------------------------------------------------------------------"
+  echo "😱 The working directory is dirty. Please commit any pending changes."
+  echo "---------------------------------------------------------------------"
+  exit 1;
 fi
 
-echo "Deleting old publication"
+echo "---------------------------"
+echo "🚮 Deleting old publication"
+echo "---------------------------"
 rm -rf public
 mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out build branch into public"
-git worktree add -B build public origin/build
+echo "----------------------------------------"
+echo "🧙🏿‍♂️ Checking out build branch into public"
+echo "----------------------------------------"
+git worktree add -B build public bitbucket/build
 
-echo "Removing existing files"
+echo "--------------------------"
+echo "🏹 Removing existing files"
+echo "--------------------------"
 rm -rf public/*
 
-echo "Generating site"
+echo "------------------"
+echo "⚙️ Generating site"
+echo "------------------"
 hugo
 
-echo "Updating gh-pages branch"
+echo "------------------------"
+echo "🧐 Updating build branch"
+echo "------------------------"
 CURRENTDATE=`date +"%D %T"`
 cd public && git add --all && git commit -m "Publishing to build ${CURRENTDATE}"
 
-# echo "Pushing to github"
-# git push --all
-git checkout build
-echo "Push to live"
+echo "Pushing to github"
+git push --all
+echo "----------------"
+echo "🚀 Push to live"
+echo "----------------"
 git-ftp push
-git checkout master
